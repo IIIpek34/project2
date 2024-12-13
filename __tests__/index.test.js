a319fb28c3fb592b604f100007118adacfd03c38
bekeshev_dmitriy <bekeshevdimasik@gmail.com>
2024-12-13 06:28:11 +0300

    fixing linter errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
-import { describe, test, expect } from '@jest/globals';
+import { describe, test, expect } from '@jest/globals'
 
 describe('User', () => {
   test('rational', () => {
-    expect();
-  });
-});
+    expect()
+  })
+})
